b2783d3b423d9cb23054de86101dfcf0ef032ecd
Amogh Suman <34958121+amoghsuman@users.noreply.github.com>
2025-04-23 15:40:44 +0530

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,11 +1,15 @@
 import json
 from datetime import datetime
+import os
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), "data", "ipl_schedule_2025_clean.json")
 
 def get_today_matches():
-    with open("app/data/ipl_schedule.json", "r") as f:
+    today = datetime.today().strftime("%Y-%m-%d")
+
+    with open(DATA_PATH, "r") as f:
         schedule = json.load(f)
 
-    today = datetime.today().strftime("%Y-%m-%d")
     today_matches = [match for match in schedule if match["date"] == today]
 
     if not today_matches:
@@ -15,4 +19,3 @@ def get_today_matches():
     for match in today_matches:
         response += f"{match['teams']} at {match['venue']} — {match['time']}\n"
     return response.strip()
-
